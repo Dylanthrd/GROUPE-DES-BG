324882b4e881e6da128ef631787d5353aaf5c9c3
JulienJbd <78910872+JulienJbd@users.noreply.github.com>
2021-02-18 17:27:09 +0100

    Revert "dylan le plouc"
    This reverts commit 84fe715e664b4839f3340cf92719d52df3d95375.

--- script.js
+++
@@ -1,8 +0,0 @@
-function couleurH1() {
-    var h1 = document.getElementById('blois');
-    couleur = document.getElementById('inputCouleurH1').value;
-    if (couleur =! '#000000') {
-        h1.setAttribute('style', 'color : ' + couleur + ';');
-        alert('La couleur ' + couleur + ' à bien été appliquée !')
-    }
-}
